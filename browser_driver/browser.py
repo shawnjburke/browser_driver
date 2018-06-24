26f489ea38eca1c3851e6756752e0bc81a729ec5
shawn.burke@teamburke.com <Correct horse battery Staple>
2018-06-24 10:48:08 -0700

    Add check_by_[xpath|id] and uncheck_by_[xpath|id] to set the state of a checkbox, but not a toggle of it.  Add a scroll_into_view method for use with checking or unchecking the box.   While it appears selenium, in the test, found the checkbox, when out of view, without it, there have been browser versions where it was required.  Decided to leave it as part of the functionality for now.
    Tests added to browser_tests for the scroll into view, and checkbox
    methods.  Added a test_html file for use with the checkbox tests.

--- browser_driver/browser.py
+++ browser_driver/browser.py
@@ -6,6 +6,8 @@ it did not."""
 
 import logging
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait, Select
+from selenium.webdriver.support import expected_conditions as ec
 
 
 class WebBrowser():
@@ -41,13 +43,55 @@ class WebBrowser():
             # If no browser is specified, first statement should handle.  This is fallback.  Maybe if browser=""
             self.driver = webdriver.Chrome()
 
+    def check_by_xpath(self, xpath):
+        """Checks a checkbox, finding it by an xpath statement.  If not selected will click() the box.  Otherwise
+        will leave the state unchanged.  This method does NOT toggle state of the checkbox.
+        """
+        element = self.driver.find_element_by_xpath(xpath)
+        if not element.is_selected():
+            if self.scroll_into_view(element):
+                element.click()
+
+    def check_by_id(self, id):
+        """Checks a checkbox, finding it by an html id value.  If not selected will click() the box.  Otherwise
+        will leave the state unchanged.  This method does NOT toggle state of the checkbox.
+        """
+        element = self.driver.find_element_by_id(id)
+
+        if not element.is_selected():
+            # Check it
+            if self.scroll_into_view(element):
+                element.click()
+
+        return element
+
+    def uncheck_by_id(self, id):
+        """Removes check on a checkbox, finding it by an html id.  If selected will click() the box to deselect.
+        Otherwise will leave the state unchanged.  This method does NOT toggle state of the checkbox.
+        """
+        element = self.driver.find_element_by_id(id)
+        if element.is_selected():
+            # Uncheck it
+            if self.scroll_into_view(element):
+                element.click()
+
+    def uncheck_by_xpath(self, xpath):
+        """Removes check on a checkbox, finding it by an xpath statement.  If selected will click() the box to deselect.
+        Otherwise will leave the state unchanged.  This method does NOT toggle state of the checkbox.
+        """
+        element = self.driver.find_element_by_xpath(xpath)
+        if element.is_selected():
+            # Uncheck it
+            if self.scroll_into_view(element):
+                element.click()
+
     def find_element_by_xpath(self, xpath):
         element = self.driver.find_element_by_xpath(xpath)
 
         return element
 
-    def find_element_by_id(self, id):
-        element = self.driver.find_element_by_xpath(id)
+    def find_element_by_id(self, element_id):
+        element = self.driver.find_element_by_id(element_id)
 
         return element
 
@@ -56,11 +100,33 @@ class WebBrowser():
 
         return element
 
+    def scroll_into_view(self, element):
+        element_in_view = False
+        
+        try:
+            wait = WebDriverWait(self.driver, 0.5)  # Timeout quickly, 1/2 second, if the element is not clickable
+            wait.until(ec.element_to_be_clickable(element))
+            element_in_view = True
+        except Exception as e:
+            # Let's bring it into view
+            self.driver.execute_script("return arguments[0].scrollIntoView();", element)
+            element_in_view = True
+
+        return element_in_view
+
     def quit(self):
         """This is a wrapper method for the native Selenium functionality.  While a user could all
         WebBrowser.driver.quit() this design allows for additional code to be done in a uniform way across all tests
         code, and it is more intuitive than having to know this object contains another object called driver."""
         self.driver.quit()
+
+    @property
+    def ip_port(self):
+        return self.driver.command_executor._conn.port
+
+    @property
+    def host(self):
+        return self.driver.command_executor._conn.host
 
     @property
     def url(self):
@@ -70,7 +136,7 @@ class WebBrowser():
     @url.setter
     def url(self, value):
         """Setting the URL will work as our navigate method in this class"""
-        self.__url = value
+        self._url = value
         self.driver.get(value)
 
     @property
